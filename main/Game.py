1a38adcae4f9b1cc5632cec0867745ece4cff6b8
KYT3C <gonzalo.garcian@e-campus.uab.cat>
2021-03-25 20:25:18 +0100

    test_gameStart

---
+++ main/Game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, player1: str, player2: str):
+        self._player1 = player1
+        self._player2 = player2
+        self._wins = ""
+
+    def wins_point(self, player: str):
+        self._wins = player
+
+    def get_score(self) -> str:
+        if self._wins == self._player1:
+            return "Fifteen-Love"
+        return "Love-All"
--- test/game_test.py
+++ test/game_test.py
@@ -1,9 +1,17 @@
 import unittest
+import sys
+
+sys.path.append('../')
+
+from main.Game import Game
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_gameStart(self):
+        game = Game("Gonzalo", "Ismael")
+        result = game.get_score()
+        assert result == "Love-All"
 
 
 if __name__ == '__main__':
